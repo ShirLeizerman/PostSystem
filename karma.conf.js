d7d7f53ee8ee84b6105b33f952c78bd045fe5ca6
ShirLeizerman <shirleizerman1@gmail.com>
2019-08-28 23:34:01 -0400

    WIP - simple issue posting system
    WIP - simple issue posting system

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/gdpr'),
+      dir: require('path').join(__dirname, './coverage/post-system'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
